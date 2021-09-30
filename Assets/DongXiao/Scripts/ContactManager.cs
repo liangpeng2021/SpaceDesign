031f9fe156e78967e00ee8c4e1eafb2076fbfd4f
hch1998310 <1225215268@qq.com>
2021-09-30 13:05:52 +0800

    触点模型

---
+++ Assets/DongXiao/Scripts/ContactManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContactManager : MonoBehaviour
+{
+    public GameObject[] chuDian;
+    public GameObject cam;
+
+    public Button jietong;
+    public Button guaduan;
+    public GameObject laidian;
+    // Start is called before the first frame update
+    void Start()
+    {
+        laidian.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //5米以外
+        {
+            for (int i = 0; i < chuDian.Length; i++)
+            {
+                chuDian[i].SetActive(true);
+                chuDian[i].GetComponent<Animator>().enabled = false;
+            }
+        }
+        //5米以内
+        {
+            for (int i = 0; i < chuDian.Length; i++)
+            {
+                chuDian[i].SetActive(true);
+                chuDian[i].GetComponent<Animator>().enabled = true;
+            }
+        }
+        //1.5米以内
+        {
+            for (int i = 0; i < chuDian.Length; i++)
+            {
+                chuDian[i].SetActive(false);
+            }
+            laidian.SetActive(true);
+        }
+    }
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
+}
