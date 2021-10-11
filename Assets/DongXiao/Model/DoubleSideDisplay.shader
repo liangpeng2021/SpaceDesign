4b9a3622ab10b59603b901a735b60cf26e56a2dc
ChenLong19940416 <59762978+ChenLong19940416@users.noreply.github.com>
2021-10-11 11:41:06 +0800

    音乐触点动效
    使用TimeLine制作的操作流程

---
+++ Assets/DongXiao/Model/DoubleSideDisplay.shader
@@ -0,0 +1,44 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "ZH/DoubleSideDisplay"{
+    Properties{
+        _MainColor("MainColor",Color) = (1,1,1,1)
+        _MainTexture("MainTexture",2D) = "white"{}
+    }
+    SubShader
+    {
+        Tags{"Queue" = "Transparent" "RenderType" = "Transparent"}
+        Blend SrcAlpha OneMinusSrcAlpha
+        Cull off //关建行
+
+        pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTexture;
+            float4 _MainColor;
+            struct v2f
+            {
+                float4 pos:POSITION;
+                float4 uv:TEXCOORD;
+            };
+            v2f vert(appdata_base v)
+            {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.vertex);
+                o.uv = v.texcoord;
+                return o;
+            }
+            half4 frag(v2f i):COLOR
+            {
+                half4 c = tex2D(_MainTexture,i.uv)*_MainColor;
+                return c;
+            }
+            ENDCG
+        }
+    }
+    FallBack "Diffuse"
+}
---
+++ Assets/ZH/Editor/CreateFolders.cs
@@ -0,0 +1,61 @@
+﻿/* Create by zh at 2019-06-25
+
+   用于创建项目的标准文件夹路径 
+
+ */
+
+using UnityEditor;
+
+public class CreateFolders : ScriptableWizard
+{
+    string rootPth = "Assets";
+    public string userName = "ZH";
+    [MenuItem("MyTools/CreateFolder")]
+    static void CreateFolder()
+    {
+        ScriptableWizard.DisplayWizard(
+            "CreateFolder", typeof(CreateFolders), "CreateAndClose");
+    }
+
+    /// <summary>
+    /// Create按钮响应
+    /// </summary>
+    void OnWizardCreate()
+    {
+        if (AssetDatabase.IsValidFolder(rootPth + "/StreamingAssets") == false)
+            AssetDatabase.CreateFolder(rootPth, "StreamingAssets");
+
+        if (AssetDatabase.IsValidFolder(rootPth + "/Plugins") == false)
+            AssetDatabase.CreateFolder(rootPth, "Plugins");
+
+        string temp = rootPth + "/Temp";
+        if (AssetDatabase.IsValidFolder(temp) == false)
+            AssetDatabase.CreateFolder(rootPth, "Temp");
+
+        if (AssetDatabase.IsValidFolder(temp + "/" + userName) == false)
+            AssetDatabase.CreateFolder(temp, userName);
+
+        string parentPth = rootPth + "/" + userName;
+
+        if (AssetDatabase.IsValidFolder(parentPth) == false)
+            AssetDatabase.CreateFolder(rootPth, userName);
+
+        string[] folders = { "Animator", "Audios", "Fonts",
+            "Materials", "Models", "MyPlugins", "Prefabs", "Resources",
+            "Scripts", "Shaders", "Scenes", "Textures", };
+        foreach(var v in folders)
+        {
+            if (AssetDatabase.IsValidFolder(parentPth + "/" + v) == false)
+                AssetDatabase.CreateFolder(parentPth, v);
+        }
+    }
+    /// <summary>
+    /// Other按钮响应
+    /// </summary>
+    void OnWizardOtherButton() { }
+
+    /// <summary>
+    /// UI界面呼出响应
+    /// </summary>
+    void OnWizardUpdate() { }
+}
