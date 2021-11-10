dafd6506ef5e3123d37fc824c8b0f2c130728900
Icetone <hun555@163.com>
2021-11-10 22:08:08 +0800

    路径忽略以及版本号更新

--- Assets/SpaceDesign/Scripts/EditorScence/PathConfig.cs
+++ Assets/SpaceDesign/Scripts/EditorScence/PathConfig.cs
@@ -20,7 +20,7 @@ namespace SpaceDesign
                 path = Application.persistentDataPath.Substring(0, Application.persistentDataPath.IndexOf("Android", StringComparison.Ordinal));
 #else
             //path = Application.streamingAssetsPath.Substring(0, Application.streamingAssetsPath.IndexOf("opporoom", StringComparison.Ordinal));
-            path = Application.streamingAssetsPath.Substring(0, Application.streamingAssetsPath.IndexOf("opporoom", StringComparison.Ordinal));
+            path = Application.streamingAssetsPath.Substring(0, Application.streamingAssetsPath.IndexOf("OPPO", StringComparison.Ordinal));
 #endif
             path = Path.Combine(path, "LenQiy", "Scences");
             if (!Directory.Exists(path))
